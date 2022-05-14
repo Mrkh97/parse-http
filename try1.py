5cb18222aa8dd6888df446c5a328efe8572e2233
Mohammadreza Khorasanchian <mkhorasanchian@gmail.com>
2022-05-14 20:48:24 +0300

    small stuff

--- try1.py
+++ try1.py
@@ -1,14 +1,26 @@
-numberOfLines=1
+numberOfLines=0
+numberOfGet=0
+numberOfPost=0
 f = open("http.log", "r")
 for x in f :
     for index,word in enumerate(x.split()):
         if word == '200':
             print(x)
-            print(x.split()[index-3])
-            print(x.split()[index+1])
+            if(x.split()[index-3])=='"GET':
+                print(x.split()[index-5])
+                print(x.split()[index+1])
+                
+                numberOfGet+=1
+            if(x.split()[index-3])=='"POST':
+                print(x.split()[index-5])
+                print(x.split()[index+1])
+                
+                numberOfPost+=1
             numberOfLines+=1
     
 
-print(numberOfLines)
+print('Total 200 requests={}'.format(numberOfLines))
+print('GET={}'.format(numberOfGet))
+print('POST={}'.format(numberOfPost))
 
 f.close()
