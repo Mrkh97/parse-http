9631d6fa4d7bf8a406e805715f967a897907ee14
Mohammadreza Khorasanchian <mkhorasanchian@gmail.com>
2022-05-22 21:13:05 +0300

    starting over

--- try1.py
+++ try1.py
@@ -12,7 +12,6 @@ numberOfPost = 0
 f = open("http.log", "r")
 w = open("result.txt", "w")
 
-
 def returnMonthNumber(arg):
     switcher = {
         "Jan": 1,
@@ -40,39 +39,30 @@ for x in f:
 
                 tarih = x.split()[index-5]
                 buyukluk = x.split()[index+1]
-                date = datetime.datetime(1000*int(tarih[8])+100*int(tarih[9])
-                                         + 10*int(tarih[10])+int(tarih[11]), returnMonthNumber(tarih[4]+tarih[5]
-                                                                                               + tarih[6]), 10*int(tarih[1])+int(tarih[2]), 10*int(tarih[13])+int(tarih[14]), 10*int(tarih[16])+int(tarih[17]), 10*int(tarih[19])+int(tarih[20])).timestamp()
+                year = 1000*int(tarih[8])+100*int(tarih[9]) + 10*int(tarih[10])+int(tarih[11])
+                month = returnMonthNumber(tarih[4] + tarih[5] + tarih[6])
+                day = 10*int(tarih[1])+int(tarih[2])
+                hour = 10*int(tarih[13])+int(tarih[14])
+                minute = 10*int(tarih[16])+int(tarih[17])
+                second = 10*int(tarih[19])+int(tarih[20])
+                date = int(datetime.datetime(year, month, day, hour, minute, second).timestamp())
                 myDict.append({'time':date,'type':'GET','size':int(buyukluk)})
-                # w.write("GET"+"\n")
-                # print(x.split()[index-5])
-                # w.write(tarih+"\n")
-
-                # print((10*int(tarih[1])+int(tarih[2])),returnMonthNumber(tarih[4]+tarih[5]
-                # +tarih[6]),1000*int(tarih[8])+100*int(tarih[9])
-                # +10*int(tarih[10])+int(tarih[11]),10*int(tarih[13])+int(tarih[14]),10*int(tarih[16])+int(tarih[17])
-                # ,10*int(tarih[19])+int(tarih[20]))
-                # print(x.split()[index+1])
-                # w.write(x.split()[index+1]+"\n")
-
                 numberOfGet += 1
             if(x.split()[index-3]) == '"POST':
                 tarih = x.split()[index-5]
                 buyukluk = x.split()[index+1]
-                date = datetime.datetime(1000*int(tarih[8])+100*int(tarih[9])
-                                         + 10*int(tarih[10])+int(tarih[11]), returnMonthNumber(tarih[4]+tarih[5]
-                                                                                               + tarih[6]), 10*int(tarih[1])+int(tarih[2]), 10*int(tarih[13])+int(tarih[14]), 10*int(tarih[16])+int(tarih[17]), 10*int(tarih[19])+int(tarih[20])).timestamp()
-                # print(date)
+                year = 1000*int(tarih[8])+100*int(tarih[9]) + 10*int(tarih[10])+int(tarih[11])
+                month = returnMonthNumber(tarih[4] + tarih[5] + tarih[6])
+                day = 10*int(tarih[1])+int(tarih[2])
+                hour = 10*int(tarih[13])+int(tarih[14])
+                minute = 10*int(tarih[16])+int(tarih[17])
+                second = 10*int(tarih[19])+int(tarih[20])
+                date = int(datetime.datetime(year, month, day, hour, minute, second).timestamp())# print(date)
                 myDict.append({'time':date,'type':'POST','size':int(buyukluk)})
-                # w.write("POST"+"\n")
-                # print(x.split()[index-5])
-                # w.write(x.split()[index-5]+"\n")
-                # print(x.split()[index+1])
-                # w.write(x.split()[index+1]+"\n")
-
                 numberOfPost += 1
             numberOfLines += 1
 
+sortedDict = sorted(myDict, key=lambda x: x['time'])
 
 w.write('Total 200 requests={}\n'.format(numberOfLines))
 w.write('GET={}\n'.format(numberOfGet))
@@ -82,29 +72,18 @@ print('Total 200 requests={}'.format(numberOfLines))
 print('GET={}'.format(numberOfGet))
 print('POST={}'.format(numberOfPost))
 
-newlist = sorted(myDict, key=lambda x: x['time'])
-for item in newlist:
-    print(item['time'])
-newDict={}
-# for item in newlist:
-    # if newDict[str(item['time'])]==null:
-    #     print('yes')
-    #     newDict[str(item['time'])]={'size':item['size'],'type':item['type']}
-    # else:
-    #     print('no')
-    #     newDict[item['time']]={'size':newDict[item['time']]}
-# for item in newlist:
-#     hashed_string = hashlib.sha256(str(item).encode('utf-8')).hexdigest()
-#     w.writelines(hashed_string+'\n'+str(item)+'\n')
-    
-stringOfNewList = str(newlist)
-# print(stringOfNewList)
-w.write(stringOfNewList)
 
+for item in sortedDict:
+    print(item)
+
+# stringOfNewList = str(newlist)
+# for item in newlist:
+#     w.write(str(item['time'])+'\n')
+# print(newlist)
 f.close()
 w.close()
 
-print(datetime.datetime.now().timestamp())
+# print(datetime.datetime.now().timestamp())
 
 
 
